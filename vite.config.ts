209ef4d842068370fb363829840752221d4cdeb5
9thReborn <silas.adinoyi@decagon.dev>
2023-05-31 13:31:24 +0100

    updated again

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "./src",
-  build: {
-    outDir: "build",
-  },
+  root: "src",
+  publicDir: "../public",
+  build: { outDir: "../build" },
   server: {
     watch: {
       usePolling: true,
